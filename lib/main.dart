fe070a34fcd1d2b7a5ac4e1c1baf08f31c152b3f
Ricardo Hernandez <ricardo.hernandez@andonix.com>
2022-04-22 21:19:05 -0500

    cambiar opcion del menú usando provider como gestor de estado

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
 import 'package:qr_reader/screens/home_screen.dart';
 import 'package:qr_reader/screens/mapa_screen.dart';
 
@@ -7,19 +9,22 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'QR Reader',
-      initialRoute: 'home',
-      routes: {
-        'home': (_) => HomeScreen(),
-        'mapa': (_) => MapaScreen(),
-      },
-      theme: ThemeData(
-          appBarTheme: AppBarTheme(backgroundColor: Colors.deepPurple),
-          floatingActionButtonTheme: FloatingActionButtonThemeData(
-            backgroundColor: Colors.deepPurple,
-          )),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => UiProvider())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'QR Reader',
+        initialRoute: 'home',
+        routes: {
+          'home': (_) => HomeScreen(),
+          'mapa': (_) => MapaScreen(),
+        },
+        theme: ThemeData(
+            appBarTheme: AppBarTheme(backgroundColor: Colors.deepPurple),
+            floatingActionButtonTheme: FloatingActionButtonThemeData(
+              backgroundColor: Colors.deepPurple,
+            )),
+      ),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
+import 'package:qr_reader/screens/direcciones_screen.dart';
+import 'package:qr_reader/screens/mapas_screen.dart';
 import 'package:qr_reader/widgets/scan_button.dart';
 
 import '../widgets/custom_navigationbar.dart';
@@ -19,12 +23,30 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Text('HomeScreen'),
-      ),
+      body: _HomeScreenBody(),
       bottomNavigationBar: CustomNavigationBar(),
       floatingActionButton: ScanButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+}
+
+class _HomeScreenBody extends StatelessWidget {
+  const _HomeScreenBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final uiProvider = Provider.of<UiProvider>(context);
+
+    final currentIndex = uiProvider.selectedMenuOpt;
+
+    switch (currentIndex) {
+      case 0:
+        return MapasScreen();
+      case 1:
+        return DireccionesScreen();
+      default:
+        return MapasScreen();
+    }
   }
 }
